556fafee37c52b1b2ac3a3c04013720c4f74dc92
Julien Ergan <julien.ergan.code@gmail.com>
2020-06-26 23:09:53 +0900

    show new and create actions - redirect and render not defined

--- app/controllers/work_groups_controller.rb
+++ app/controllers/work_groups_controller.rb
@@ -1,3 +1,33 @@
 class WorkGroupsController < ApplicationController
 
+  def show
+    @work_group = WorkGroup.find(params[:id])
+    # authorize @work_group
+  end
+
+
+  def new
+    @classroom = Classroom.find(params[:classroom_id])
+    @work_group = WorkGroup.new
+    # authorize @work_group
+  end
+
+  def create
+    @classroom = Classroom.find(params[:classroom_id])
+    @work_group = WorkGroup.new(work_group_params)
+    @work_group.classroom = @classroom
+    if @work_group.save
+      # redirect_to TO DEFINE
+    else
+      # render json
+    end
+  end
+
+
+   private
+
+  def work_group_params
+    params.require(:work_group).permit(:name, :video_call_code, :classroom_id)
+  end
+
 end
