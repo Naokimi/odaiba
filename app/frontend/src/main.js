b3e7b7f44e353639836ae50cc062e3ef8eb439fd
dzakki <dzakkiaz7@gmail.com>
2020-06-26 20:54:00 +0700

    breakoutroom-html-vue

--- app/frontend/src/main.js
+++ app/frontend/src/main.js
@@ -1,12 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import Axios from "axios";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-Vue.config.productionTip = false
+Axios.defaults.baseURL = "http://localhost:3000";
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
---
+++ app/frontend/src/views/teachers/Login.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <input type="text" v-model="username" />
+    <input type="text" v-model="password" />
+    <button @click="login">Login</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LoginTeacher",
+
+  data() {
+    return {
+      username: "",
+      password: "",
+    };
+  },
+  methods: {
+    login() {
+      this.$store.dispatch("login", {
+        username: this.username,
+        password: this.password,
+        role: "teacher",
+      });
+    },
+  },
+};
+</script>
+
+<style lang="css" scoped></style>
