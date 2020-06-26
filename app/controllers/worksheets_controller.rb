7259054a0853f17dba2e88679ea838654665c6ba
Julien Ergan <julien.ergan.code@gmail.com>
2020-06-27 00:15:21 +0900

    generated worksheets controller, coded show edit and update actions - without render json

---
+++ app/controllers/worksheets_controller.rb
@@ -0,0 +1,31 @@
+class WorksheetsController < ApplicationController
+
+  def show
+    @worksheet = Worksheet.find(params[:id])
+     # authorize @worksheet
+  end
+
+  def edit
+    @classroom = Classroom.find(params[:classroom_id])
+    @work_group = WorkGroup.find(params[:work_group])
+    @worksheet = Worksheet.find(params[:id])
+  end
+
+
+  def update
+    @classroom = Classroom.find(params[:classroom_id])
+    @work_group = WorkGroup.find(params[:work_group])
+    @worksheet = Worksheet.find(params[:id])
+    if @worksheet.update(worksheet_params)
+      # TODO redirect using JSON ?
+    else
+      render :edit
+  end
+
+  private
+
+  def worksheet_params
+    params.require(:worksheet).permit(:work_group_id)
+  end
+
+end
