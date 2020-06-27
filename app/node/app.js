200b72c849c39d445e413bd37f3e7bfd6d9bf0ab
dzakki <dzakkiaz7@gmail.com>
2020-06-27 21:09:42 +0700

    realtime_task

--- app/node/app.js
+++ app/node/app.js
@@ -1,57 +1,21 @@
-const express = require("express");
-const app = express();
-const port = 3000;
+const app = require("express")();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
 
-const {
-  RtcTokenBuilder,
-  RtmTokenBuilder,
-  RtcRole,
-  RtmRole,
-} = require("agora-access-token");
+app.get("/", (req, res) => {
+  res.send("hello there");
+});
 
-app.post("/token", (req, res) => {
-  const appID = "facc2f66a4394574a83a612d93852cb9";
-  const appCertificate = '"734be6d2bea74a9c8b9d5be7f8acf19d"';
-  const channelName = "room1";
-  const uid = 2882341273;
-  const account = "2882341273";
-  const role = RtcRole.PUBLISHER;
-  const expirationTimeInSeconds = 3600;
-
-  const currentTimestamp = Math.floor(Date.now() / 1000);
-  const privilegeExpiredTs = currentTimestamp + expirationTimeInSeconds;
-
-  // return RtcTokenBuilder.buildTokenWithAccount(
-  //   appID,
-  //   appCertificate,
-  //   channelName,
-  //   uid,
-  //   role,
-  //   privilegeExpiredTs
-  // ); = currentTimestamp + expirationTimeInSeconds;
-  // console.log(channelName);
-  const token = RtcTokenBuilder.buildTokenWithAccount(
-    appID,
-    appCertificate,
-    channelName,
-    uid,
-    role,
-    privilegeExpiredTs
-  );
-
-  // const token = RtcTokenBuilder.buildTokenWithAccount(
-  //   appID,
-  //   appCertificate,
-  //   channelName,
-  //   account,
-  //   role,
-  //   privilegeExpiredTs
-  // );
-  res.status(201).json({
-    token,
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("answerQ", function(response) {
+    console.log(response);
+    socket.broadcast.emit("answerQ", response);
   });
 });
 
-app.listen(port, () =>
-  console.log(`Example app listening at http://localhost:${port}`)
-);
+const PORT = process.env.PORT || 3001;
+
+http.listen(PORT, () => {
+  console.log("listening on *:", process.env.PORT || 3001);
+});
