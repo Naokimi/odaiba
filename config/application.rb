2efbd2d8854ade3a6c24f1302a8936a74b212652
dzakki <dzakkiaz7@gmail.com>
2020-06-28 02:46:43 +0700

    enabled_cors

--- config/application.rb
+++ config/application.rb
@@ -27,12 +27,15 @@ module Odaiba
     config.load_defaults 6.0
 
     
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins '*'
-        resource '*', headers: :any, methods: :any
-      end
-    end
+    # enabled cors
+    # please install rack-cors and add rack-cors to Gemfile
+    # config.middleware.insert_before 0, Rack::Cors do
+    #   allow do
+    #     origins '*'
+    #     resource '*', headers: :any, methods: :any
+    #   end
+    # end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
