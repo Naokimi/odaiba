c7ff3eb50d8e875f15e28e3dc79427c20a46d5b4
Julien Ergan <julien.ergan.code@gmail.com>
2020-06-27 23:36:32 +0900

    authorisation setup with gem Devise following Le Wagon Tokyo Kitt lecture

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_27_013414) do
+ActiveRecord::Schema.define(version: 2020_06_27_143141) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,20 @@ ActiveRecord::Schema.define(version: 2020_06_27_013414) do
     t.bigint "teacher_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+    t.string "subject"
+    t.datetime "start_time"
+    t.datetime "end_time"
     t.index ["teacher_id"], name: "index_classrooms_on_teacher_id"
+  end
+
+  create_table "group_work_sheets", force: :cascade do |t|
+    t.bigint "work_group_id", null: false
+    t.bigint "worksheet_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["work_group_id"], name: "index_group_work_sheets_on_work_group_id"
+    t.index ["worksheet_id"], name: "index_group_work_sheets_on_worksheet_id"
   end
 
   create_table "student_work_groups", force: :cascade do |t|
@@ -43,27 +56,41 @@ ActiveRecord::Schema.define(version: 2020_06_27_013414) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
   create_table "work_groups", force: :cascade do |t|
     t.string "name"
     t.string "video_call_code"
     t.bigint "classroom_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "time_limit"
+    t.integer "rotation_time"
     t.index ["classroom_id"], name: "index_work_groups_on_classroom_id"
   end
 
   create_table "worksheets", force: :cascade do |t|
-    t.bigint "work_group_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.json "display_content"
     t.json "correct_content"
-    t.index ["work_group_id"], name: "index_worksheets_on_work_group_id"
+    t.string "name"
   end
 
   add_foreign_key "classrooms", "teachers"
+  add_foreign_key "group_work_sheets", "work_groups"
+  add_foreign_key "group_work_sheets", "worksheets"
   add_foreign_key "student_work_groups", "students"
   add_foreign_key "student_work_groups", "work_groups"
   add_foreign_key "work_groups", "classrooms"
-  add_foreign_key "worksheets", "work_groups"
 end
