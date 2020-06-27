6fae5b06fdc7019bc6edec128e073b1dbf453ce2
dzakki <dzakkiaz7@gmail.com>
2020-06-27 21:09:30 +0700

    realtime_task

--- app/frontend/src/store/workSheets.js
+++ app/frontend/src/store/workSheets.js
@@ -1,8 +1,90 @@
 import Axios from "axios";
+import io from "socket.io-client";
+const socket = io("http://localhost:3001");
 
 export default {
   namespaced: true,
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    sheets: [],
+  },
+  mutations: {
+    SET_SHEETS(state, payload) {
+      this.sheets = payload;
+    },
+    INPUT_ANSWER(state, payload) {
+      /* {
+        indexArray,
+        answer,
+        code: code == `verb-2` ? 2 : 3,
+        idQuestion
+      }*/
+      console.log(payload)
+      let sheetsTemp = JSON.stringify(state.sheets);
+      sheetsTemp = JSON.parse(sheetsTemp);
+      console.log(sheetsTemp);
+      sheetsTemp[payload.indexArray].questions[payload.code] = payload.answer;
+      // sheetsTemp[payload.indexArray].idKey = Math.random()
+      state.sheets = sheetsTemp
+    },
+  },
+  actions: {
+    getWorkSheets({ state, commit }, id) {
+      if (id > 6) {
+        return;
+      }
+      Axios.get(`/classrooms/2/work_groups/2/worksheets/${id}.json`)
+        .then(({ data }) => {
+          const sheets = state.sheets;
+          // console.log(JSON.parse(data.display_content));
+
+          // console.log(JSON.parse(data.correct_content));
+          const questions = JSON.parse(data.display_content);
+          console.log(questions);
+          const answers = JSON.parse(data.correct_content);
+          const sheetsTemp = [
+            {
+              questions: questions[1],
+              answers: answers[1],
+              id: data.id,
+              idKey: Math.random()
+            },
+            {
+              questions: questions[2],
+              answers: answers[2],
+              id: data.id,
+              idKey: Math.random()
+            },
+          ];
+          // console.log(sheet);
+          sheets.push(...sheetsTemp);
+          console.log(sheets);
+          commit("SET_SHEETS", sheets);
+        })
+        .catch((err) => console.log(err));
+    },
+    AnswerQuestion({ commit, state }, payload) {
+      commit("INPUT_ANSWER", payload);
+      console.log(payload);
+      socket.emit("answerQ", payload);
+    },
+    UpdateAnswer({ commit }) {
+      socket.on("answerQ", function(response) {
+        console.log(response);
+        commit("INPUT_ANSWER", response);
+      });
+    },
+  },
 };
+
+// [
+//   sheet: {
+//     questions: [
+//       ["hashiru", "run", false, false],
+//       ["iu", "say", false, false]
+//     ],
+//     answers: [
+//        ["hashiru", "run", "ran", "run"]
+//        ["iu", "say", "said", "said"]
+//     ]
+//   }
+// ]
--- app/frontend/src/views/students/BreakoutGroup.vue
+++ app/frontend/src/views/students/BreakoutGroup.vue
@@ -1,9 +1,12 @@
 <template>
   <div>
     <Navbar />
+
     <div class="flex p-6">
-      <a @click.prevent="$router.go(-1)">
-        <div class="flex-none h-12 w-12 text-center bg-white hover:bg-gray-200 p-3 m-2 rounded-full">
+      <a href="#">
+        <div
+          class="flex-none h-12 w-12 text-center bg-white hover:bg-gray-200 p-3 m-2 rounded-full"
+        >
           <i class="fas fa-reply text-gray-700"></i>
         </div>
       </a>
@@ -11,11 +14,57 @@
       <div class="flex-initial w-full text-gray-700 text-center m-2">
         <div class="flex mb-4">
           <div class="w-8/12 px-4">
-            <div class="bg-white p-4 rounded-lg">Worksheet goes here</br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></div>
+            <div class="bg-white p-4 rounded-lg">
+              <div class="bg-gray-200 p-4 rounded">
+                <table class="table-fixed bg-white">
+                  <thead>
+                    <tr>
+                      <th class="border w-1/12 px-4 py-2"></th>
+                      <th class="border w-2/12 px-4 py-2">日本語</th>
+                      <th class="border w-3/12 px-4 py-2">原形</th>
+                      <th class="border w-3/12 px-4 py-2">過去形</th>
+                      <th class="border w-3/12 px-4 py-2">過去分詞形</th>
+                    </tr>
+                  </thead>
+                  <tbody class="text-sm">
+                    <tr class="font-medium">
+                      <td class="border px-4 py-2">例</td>
+                      <td class="border px-4 py-2">始める</td>
+                      <td class="border px-4 py-2">begin</td>
+                      <td class="border px-4 py-2">began</td>
+                      <td class="border px-4 py-2">begun</td>
+                    </tr>
+                    <tr v-for="(sheet, i) in sheets" :key="sheet.idKey">
+                      <td class="border px-4 py-4">{{ i + 1 }}</td>
+                      <td class="border px-4 py-4">{{ sheet.questions[0] }}</td>
+                      <td class="border px-4 py-4">{{ sheet.questions[1] }}</td>
+                      <td class="border px-4 py-4">
+                        <InputAnswer
+                          :code="`verb-2`"
+                          :idQuestion="sheet.id"
+                          :indexArray="i"
+                          :answerTemp="sheet.questions[2]"
+                        />
+                      </td>
+                      <td class="border px-4 py-4">
+                        <InputAnswer
+                          :code="`verb-3`"
+                          :idQuestion="sheet.id"
+                          :indexArray="i"
+                          :answerTemp="sheet.questions[3]"
+                        />
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
           </div>
           <div class="w-4/12 px-4">
-
-            <div class="block bg-orange-500 p-3 rounded-lg mb-6" style="background-color: #F98436">
+            <div
+              class="block bg-orange-500 p-3 rounded-lg mb-6"
+              style="background-color: #F98436"
+            >
               <div class="font-semibold text-xl text-white mb-2">
                 <h4>Timer</h4>
               </div>
@@ -34,31 +83,47 @@
             <div class="block bg-white p-3 rounded-lg mb-8">
               <div class="font-semibold text-xl mb-3">
                 <h4 class="mb-2">Members</h4>
-                <hr class="border">
+                <hr class="border" />
               </div>
               <div class="bg-white rounded-lg">
-                <div class="block w-full bg-gray-200 px-4 py-3 text-left items-center mb-2">
-                  <img class="w-8 mr-4 bg-white rounded-full inline"
+                <div
+                  class="block w-full bg-gray-200 px-4 py-3 text-left items-center mb-2"
+                >
+                  <img
+                    class="w-8 mr-4 bg-white rounded-full inline"
                     src="https://s3.us-west-2.amazonaws.com/secure.notion-static.com/c3fa12ba-dbc1-4c80-bb2b-efcb1e8a4273/icons8-finn-96.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAT73L2G45O3KS52Y5%2F20200626%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20200626T110244Z&X-Amz-Expires=86400&X-Amz-Signature=0fc60a68d61741cfb3b9aac08959cbcd5c8b25db5f409fdadc98ccf77b3a778c&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22icons8-finn-96.png%22"
-                    alt="student-avatar" >
+                    alt="student-avatar"
+                  />
                   <p class="text-lg inline">Audrey</p>
                 </div>
-                <div class="block w-full bg-gray-200 px-4 py-3  text-left items-center mb-2">
-                  <img class="w-8 mr-4 bg-white rounded-full inline"
+                <div
+                  class="block w-full bg-gray-200 px-4 py-3  text-left items-center mb-2"
+                >
+                  <img
+                    class="w-8 mr-4 bg-white rounded-full inline"
                     src="https://s3.us-west-2.amazonaws.com/secure.notion-static.com/c3fa12ba-dbc1-4c80-bb2b-efcb1e8a4273/icons8-finn-96.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAT73L2G45O3KS52Y5%2F20200626%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20200626T110244Z&X-Amz-Expires=86400&X-Amz-Signature=0fc60a68d61741cfb3b9aac08959cbcd5c8b25db5f409fdadc98ccf77b3a778c&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22icons8-finn-96.png%22"
-                    alt="student-avatar" >
+                    alt="student-avatar"
+                  />
                   <p class="text-lg inline">Benson</p>
                 </div>
-                <div class="block w-full bg-gray-200 px-4 py-3  text-left items-center mb-2">
-                  <img class="w-8 mr-4 bg-white rounded-full inline"
+                <div
+                  class="block w-full bg-gray-200 px-4 py-3  text-left items-center mb-2"
+                >
+                  <img
+                    class="w-8 mr-4 bg-white rounded-full inline"
                     src="https://s3.us-west-2.amazonaws.com/secure.notion-static.com/c3fa12ba-dbc1-4c80-bb2b-efcb1e8a4273/icons8-finn-96.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAT73L2G45O3KS52Y5%2F20200626%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20200626T110244Z&X-Amz-Expires=86400&X-Amz-Signature=0fc60a68d61741cfb3b9aac08959cbcd5c8b25db5f409fdadc98ccf77b3a778c&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22icons8-finn-96.png%22"
-                    alt="student-avatar" >
+                    alt="student-avatar"
+                  />
                   <p class="text-lg inline">Carly</p>
                 </div>
-                <div class="block w-full bg-gray-200 px-4 py-3  text-left items-center mb-2">
-                  <img class="w-8 mr-4 bg-white rounded-full inline"
+                <div
+                  class="block w-full bg-gray-200 px-4 py-3  text-left items-center mb-2"
+                >
+                  <img
+                    class="w-8 mr-4 bg-white rounded-full inline"
                     src="https://s3.us-west-2.amazonaws.com/secure.notion-static.com/c3fa12ba-dbc1-4c80-bb2b-efcb1e8a4273/icons8-finn-96.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAT73L2G45O3KS52Y5%2F20200626%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20200626T110244Z&X-Amz-Expires=86400&X-Amz-Signature=0fc60a68d61741cfb3b9aac08959cbcd5c8b25db5f409fdadc98ccf77b3a778c&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22icons8-finn-96.png%22"
-                    alt="student-avatar" >
+                    alt="student-avatar"
+                  />
                   <p class="text-lg inline">Darwin</p>
                 </div>
               </div>
@@ -66,29 +131,34 @@
 
             <div class="block flex flex-wrap">
               <a class="w-1/3 ml-auto" href="#">
-                <div class="flex-none h-20 w-20 text-center bg-white hover:bg-orange-500 p-6 ml-6 rounded-full">
+                <div
+                  class="flex-none h-20 w-20 text-center bg-white hover:bg-orange-500 p-6 ml-6 rounded-full"
+                >
                   <i class="fas fa-hand-paper fa-2x text-gray-700"></i>
                 </div>
               </a>
               <a class="w-1/3 mr-auto" href="#">
-                <div class="flex-none h-20 w-20 text-center bg-white hover:bg-orange-500 py-6 px-5 ml-6 rounded-full">
+                <div
+                  class="flex-none h-20 w-20 text-center bg-white hover:bg-orange-500 py-6 px-5 ml-6 rounded-full"
+                >
                   <i class="fas fa-microphone-slash fa-2x text-gray-700"></i>
                 </div>
               </a>
             </div>
-
           </div>
         </div>
-
       </div>
     </div>
-    <AaudioBreakoutRoom/>
+    <AaudioBreakoutRoom />
   </div>
 </template>
 
 <script>
 import Navbar from "@/components/Navbar.vue";
-import AaudioBreakoutRoom  from "@/components/AaudioBreakoutRoom.vue"
+import AaudioBreakoutRoom from "@/components/AaudioBreakoutRoom.vue";
+import InputAnswer from "@/components/InputAnswer.vue";
+
+import { mapActions, mapState } from "vuex";
 
 export default {
   name: "BreakoutGroup",
@@ -97,8 +167,22 @@ export default {
   },
   components: {
     Navbar,
-    AaudioBreakoutRoom
-  }
+    AaudioBreakoutRoom,
+    InputAnswer,
+  },
+  computed: {
+    ...mapState("workSheets", ["sheets"]),
+  },
+  methods: {
+    ...mapActions("workSheets", ["getWorkSheets", "UpdateAnswer"]),
+    answerQ() {
+      //answer question
+    },
+  },
+  created() {
+    this.getWorkSheets(1);
+    this.UpdateAnswer();
+  },
 };
 </script>
 
