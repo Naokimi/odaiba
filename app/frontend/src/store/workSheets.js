855f70f3c16ca87fdc78743e87a7a544ce086548
dzakki <dzakkiaz7@gmail.com>
2020-06-28 13:40:57 +0700

    url

--- app/frontend/src/store/workSheets.js
+++ app/frontend/src/store/workSheets.js
@@ -1,6 +1,7 @@
 import Axios from "axios";
 import io from "socket.io-client";
-const socket = io("http://localhost:3001");
+import { socket_url } from "../url";
+const socket = io(socket_url);
 
 export default {
   namespaced: true,
--- app/frontend/src/url.js
+++ app/frontend/src/url.js
@@ -1,2 +1,2 @@
-// export const socket_url = `https://socket-odaiba.herokuapp.com`;
-export const socket_url = `http://localhost:3001`;
+export const socket_url = `https://socket-odaiba.herokuapp.com`;
+// export const socket_url = `http://localhost:3001`;
